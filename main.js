2988c1a4bfa07e098b3a298232c827655460454d
zarazahed <158002909+zarazahed@users.noreply.github.com>
2025-10-05 23:20:18 +0000

    finished part 3

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ gainNode.connect(audioCtx.destination);
 oscillator.type = "sine";
 oscillator.start();
 gainNode.gain.value = 0;
+var amplitude = 40;
+
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d"); 
+var width = ctx.canvas.width;
+var height = ctx.canvas.height;
 
 notenames = new Map();
 notenames.set("C", 261.6);
@@ -18,6 +25,16 @@ notenames.set("F", 349.2);
 notenames.set("G", 392.0);
 notenames.set("A", 440);
 notenames.set("B", 493.9);
+
+function line() {
+    freq = pitch / 10000;
+
+   y = height/2 + (amplitude * Math.sin(x * 2 * Math.PI * freq));
+   ctx.lineTo(x, y);
+   ctx.stroke();
+   x = x + 1;
+}
+
 
 function frequency(pitch) {
     gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
