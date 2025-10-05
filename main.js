dd4ae79b41bb9ba9268a52c8c75854cb29397464
zarazahed <158002909+zarazahed@users.noreply.github.com>
2025-10-05 16:54:29 +0000

    finished part 2

---
+++ main.js
@@ -0,0 +1,34 @@
+const input = document.getElementById('input');
+
+const audioCtx = new AudioContext();
+const gainNode = audioCtx.createGain();
+
+const oscillator = audioCtx.createOscillator();
+oscillator.connect(gainNode);
+gainNode.connect(audioCtx.destination);
+oscillator.type = "sine";
+oscillator.start();
+gainNode.gain.value = 0;
+
+notenames = new Map();
+notenames.set("C", 261.6);
+notenames.set("D", 293.7);
+notenames.set("E", 329.6);
+notenames.set("F", 349.2);
+notenames.set("G", 392.0);
+notenames.set("A", 440);
+notenames.set("B", 493.9);
+
+function frequency(pitch) {
+    gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
+    oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
+    gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
+}
+
+function handle() {
+    var usernotes = String(input.value);
+    frequency(notenames.get(usernotes)); s
+    audioCtx.resume();
+    gainNode.gain.value = 0;
+   frequency(input.value);
+}
